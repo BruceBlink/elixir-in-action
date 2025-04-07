8346bcdec00ff82973e14ff9cfcdde0266a51e0d
likanug <likanug.g@qq.com>
2025-04-07 14:09:51 +0800

    重构 TodoServer 和 TodoList 模块，添加条目管理和查询功能

--- todo_server.ex
+++ todo_server.ex
@@ -1,7 +1,49 @@
 defmodule TodoServer do
+  def start do
+    spawn(fn -> loop(TodoList.new()) end)
+  end
 
+  defp loop(todo_list) do
+    new_todo_list =
+      receive do
+        message -> process_message(todo_list, message)
+      end
+    loop(new_todo_list)
+  end
+
+  defp process_message(todo_list, {:entries, caller, date}) do
+    send(caller, {:todo_entries, TodoList.entries(todo_list, date)})
+    todo_list
+  end
+
+  def add_entry(server, new_entry) do
+    send(server, {:add_entry, new_entry})
+  end
+
+  def entries(todo_server, date) do
+    send(server, {:get_entries, self(), date})
+    receive do
+      {:todo_entries, entries} -> entries
+    after
+      5000 -> {:error, :timeout}
+    end
+  end
 end
 
 defmodule TodoList do
+  defstruct auto_id: 1, entries: %{}
 
+  def new do
+    %TodoList{}
+  end
+
+  def add_entry(todo_list, entry) do
+    entry = Map.put(entry, :id, todo_list.auto_id)
+    new_entries = Map.put(todo_list.entries, todo_list.auto_id, entry)
+    %{todo_list | auto_id: todo_list.auto_id + 1, entries: new_entries}
+  end
+
+  def entries(todo_list) do
+    todo_list.entries
+  end
 end
