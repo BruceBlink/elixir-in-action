35c02ed0f85e20c3f67e7a4c160c3a4d268614d8
likanug <likanug.g@qq.com>
2025-04-09 12:20:33 +0800

    重构 ServerProcess 模块，增加 :caller 请求类型的支持，并调整 put 方法为使用 cast 进行请求

--- server_process.ex
+++ server_process.ex
@@ -8,20 +8,28 @@ defmodule ServerProcess do
 
   defp loop(callback_module, state) do
     receive do
-      {request, caller} ->
+      {:call, request, caller} ->    # 增加一个 :caller请求类型
         {response, new_state} =
           callback_module.handle_call(request, state)  # 调用回调模块的 handle_call处理请求
           send(caller, {:response, response})  # 发送响应给调用者
+          loop(callback_module, new_state)  # 继续循环，使用新的状态
+      {:cast, request} -> # 处理 cast 请求
+        new_state =
+          callback_module.handle_cast(request, state)  # 调用回调模块的 handle_cast处理请求
           loop(callback_module, new_state)  # 继续循环，使用新的状态
     end
   end
 
   def call(server_pid, request) do
-    send(server_pid, {request, self()})  # 发送请求和调用者的 PID
+    send(server_pid, {:call, request, self()})  # 发送请求和调用者的 PID, 增加一个 :caller请求类型
     receive do
       {:response, response} -> # 等待响应
         response  # 返回响应
     end
+  end
+
+  def cast(server_pid, request) do
+    send(server_pid, {:cast, request})  # 发送请求
   end
 
 end
@@ -43,12 +51,16 @@ defmodule KeyValueStore do
     # {Map.get(state, key) ,state}
   end
 
+  def handle_cast({:put, key, value}, state) do
+    Map.put(state, key, value)  # 更新状态
+  end
+
   def start do
     ServerProcess.start(__MODULE__)  # 启动服务器进程，传入当前模块作为回调模块
   end
 
   def put(server_pid, key, value) do
-    ServerProcess.call(server_pid, {:put, key, value})  # 调用服务器进程的 put 方法
+    ServerProcess.cast(server_pid, {:put, key, value})  # 调用服务器进程的 put 方法
   end
 
   def get(server_pid, key) do
