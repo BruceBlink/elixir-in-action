490000b92d932d60ee8aa879d3ecad6e834c7f3d
likanug <likanug.g@qq.com>
2025-04-09 10:02:22 +0800

    重构 ServerProce 模块，添加 loop 函数以处理请求和状态管理

--- server_process.ex
+++ server_process.ex
@@ -6,4 +6,14 @@ defmodule ServerProce do
     end)
   end
 
+  defp loop(callback_module, state) do
+    receive do
+      {request, caller} ->
+        {response, new_state} =
+          callback_module.handle_call(request, state)  # 调用回调模块的 handle_call处理请求
+          send(caller, {:response, response})  # 发送响应给调用者
+          loop(callback_module, new_state)  # 继续循环，使用新的状态
+    end
+  end
+
 end
