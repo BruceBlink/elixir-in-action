e08b6aa396c9d332d3ed439b5292b6996a8b04ac
likanug <likanug.g@qq.com>
2025-04-10 15:15:34 +0800

    添加 KeyValStore 和 CleanupServer 模块，支持定时清理功能

--- key_value_gen_server.ex
+++ key_value_gen_server.ex
@@ -24,3 +24,39 @@ defmodule KeyValueStore do
         GenServer.call(server_pid, {:get, key})
     end
 end
+
+defmodule KeyValStore do
+    use GenServer
+
+    def init(_) do
+        :timer.send_interval(5000, :cleanup)
+        {:ok, %{}}
+    end
+
+    def handle_info(:cleanup, state) do
+        IO.puts "preforming cleanup..."
+        {:noreply, state}
+    end
+end
+
+defmodule CleanupServer do
+    use GenServer
+
+    # 初始化时启动定时器
+    def init(_) do
+      :timer.send_interval(3000, self(), :cleanup)  # 每30秒发送:cleanup消息
+      {:ok, %{data: [], last_cleanup: nil}}
+    end
+
+    # 处理普通消息
+    def handle_info(:cleanup, state) do
+      new_state = do_cleanup(state)
+      IO.puts "#{new_state.last_cleanup} preforming cleanup..."
+      {:noreply, new_state}
+    end
+
+    defp do_cleanup(state) do
+      # 清理过期数据
+      %{state | data: [], last_cleanup: DateTime.utc_now()}
+    end
+end
